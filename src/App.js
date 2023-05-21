5cef7b3c5c17533699f3b75ec5f52e847c357b80
Manuel Torres <manueljosetorres02@gmail.com>
2023-05-20 20:00:45 -0500

    basic stylings.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+const Button = styled.button`
+  display: inline-block;
+  flex: 1;
+  border: none;
+  background-color: teal;
+  color: white;
+  height: 30px;
+  width: 50px;
+  border-radius: 2px;
+  cursor: pointer;
+`;
+const Text = styled.input`
+  border: 2px solid #000;
+`;
+const TaskCount = styled.span`
+  margin: 10px;
+`;
+const Tasks = styled.div``;
+const TaskCount = styled.span`
+  margin: 10px;
+`;
+const Tasks = styled.div``;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div>
+        <h2>Todo List</h2>
+        <Text value={input} />
+        <Button>Add</Button>
+        <Tasks>
+          <TaskCount>
+            <b>Pending Tasks</b>
+          </TaskCount>
+          <TaskCount>
+            <b>Completed Tasks</b>
+          </TaskCount>
+        </Tasks>
+        <div>
+          <ul>/* List items consisting of tasks will be listed here */</ul>
+        </div>
+        <Button>Clear</Button>
+      </div>
+    </Container>
   );
 }
-
 export default App;
